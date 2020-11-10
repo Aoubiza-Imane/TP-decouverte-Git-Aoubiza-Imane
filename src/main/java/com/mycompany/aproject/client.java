4c560eb39c6d876f1992978a5b5b2d06ab2c5449
kooki <kooki@192.168.0.16>
2020-11-10 14:39:11 +0100

    mon first commit ever

---
+++ src/main/java/com/mycompany/aproject/client.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.aproject;
+
+/**
+ *
+ * @author kooki
+ */
+public class client {
+    // salut ceci est une classe java
+    
+}
